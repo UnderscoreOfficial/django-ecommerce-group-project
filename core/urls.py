e6e606f75fa78866b9e808fce032141ad4fee29a
Robby Attila <warpscroll@gmail.com>
2023-05-04 20:10:44 -0700

    integrated bootstrap and added basic url routing

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from .views import homepage
+from django.urls import path, include
 
 urlpatterns = [
-    path('', homepage),
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('', include('cart.urls')),
+    path('', include('checkout.urls')),
+    path('', include('orders.urls')),
+    path('', include('products.urls')),
 ]
